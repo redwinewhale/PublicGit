af3f67d0b7c7c8696ac84f3c9e6ece67e88e0899
Sky_Whale <dbsdnjswn123@gmail.com>
2025-03-26 16:43:34 +0900

    update

--- test/test.cpp
+++ test/test.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main() {
     cout << "Hello World!" << endl;
-    cout << "I am a C++ program." << endl;
+    cout << "I am a C++ program. OK?" << endl;
     return 0;
 }
